2a27b731629b93761895f902f2ab609e866f425a
Tore Hirth <torehirth@gmail.com>
2025-08-04 15:21:30 +0200

    create router function

---
+++ src/js/app.mjs
@@ -0,0 +1,47 @@
+const router = () => {
+  const path = window.location.pathname;
+
+  switch (path) {
+    case "/index.html":
+    case "/":
+      // functions..
+      break;
+
+    case "/login":
+    case "/login/index.html":
+      // Functions..
+      break;
+
+    case "/register":
+    case "/register/index.html":
+      // Functions..
+      break;
+
+    case "/auctions":
+    case "/auctions/index.html":
+      // Functions..
+      break;
+
+    case "/about":
+    case "/about/index.html":
+      // Functions..
+      break;
+
+    case "/contact":
+    case "/contact/index.html":
+      // Functions..
+      break;
+
+    case "/profile":
+    case "/profile/index.html":
+      // Functions..
+      break;
+
+    case "/new-listing":
+    case "/new-listing/index.html":
+      // Functions..
+      break;
+  }
+};
+
+router();
