e5f9b06d7dc7e8459a037828d2367bc441474307
Tore Hirth <torehirth@gmail.com>
2025-08-04 20:49:09 +0200

    add routes object in separate file

---
+++ src/js/router/routes.mjs
@@ -0,0 +1,10 @@
+export const routes = {
+  "/": renderHome,
+  "/login": renderLogin,
+  "/register": renderRegister,
+  "/auctions": renderAuctions,
+  "/auctions/:id": renderListing,
+  "/profile": renderProfile,
+  "/about": renderAbout,
+  "/contact": renderContact,
+};
