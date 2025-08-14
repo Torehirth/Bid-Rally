74bacbe2fc414520128d13c8db6545bfa24b5cc9
Tore Hirth <torehirth@gmail.com>
2025-08-14 23:19:36 +0200

    create separate functions for fetching API, render and displaying auction listings

--- src/js/pages/auctions.mjs
+++ src/js/pages/auctions.mjs
@@ -1,3 +1,24 @@
-export const initAuctionsPage = () => {
+import renderAuctionCards from "../shared/ui/auctions/renderAuctionCards.mjs";
+import fetchAPI from "../shared/api/fetchAPI.mjs";
+
+export const initAuctionsPage = async () => {
   console.log("init Auctions page");
+  auctionCardsHandler();
+};
+
+const auctionCardsHandler = async () => {
+  const container = document.querySelector("#auctions-grid");
+
+  try {
+    const json = await fetchAPI(
+      container,
+      "listings",
+      "_active&_seller&_bids&limit=10"
+    );
+
+    const allAuctions = json?.data;
+    renderAuctionCards(allAuctions, container);
+  } catch (err) {
+    container.innerHTML = err;
+  }
 };
---
+++ src/js/shared/api/fetchAPI.mjs
@@ -0,0 +1,21 @@
+const fetchAPI = async (container, endpoint, parameter = "") => {
+  const baseAPIUrl = import.meta.env.VITE_API_BASE_URL;
+
+  try {
+    const response = await fetch(`${baseAPIUrl}/${endpoint}?${parameter}`);
+
+    if (!response.ok) {
+      throw new Error(json.errors?.[0]?.message || "Fetch failed");
+    }
+
+    const json = await response.json();
+
+    return json;
+  } catch (err) {
+    container.innerHTML =
+      "OOOps! Error when fetching the API! Try again later..";
+    console.error(err);
+  }
+};
+
+export default fetchAPI;
