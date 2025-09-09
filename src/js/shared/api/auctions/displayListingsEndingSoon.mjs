00cb9349e029f7fd2f91affd7b3f1a3ee95449bd
Tore Hirth <torehirth@gmail.com>
2025-09-09 22:01:14 +0200

    update jsdocs

--- src/js/shared/api/auctions/displayListingsEndingSoon.mjs
+++ src/js/shared/api/auctions/displayListingsEndingSoon.mjs
@@ -2,6 +2,18 @@ import { renderAuctionCards } from "../../ui/auctions/auctionCards/renderAuction
 import { displayMessage } from "../../utils/common/displayMessage.mjs";
 import { fetchAPI } from "../fetchAPI.mjs";
 
+/**
+ * Fetches and displays auction listings that are ending soon.
+ *
+ * @async
+ * @function displayListingsEndingSoon
+ * @param {string} [sort="endsAt"] - The field by which to sort the listings.
+ * @param {string} [sortOrder="asc"] - The order in which to sort the listings ("asc" for ascending, "desc" for descending).
+ * @param {number} [limit=4] - The maximum number of listings to fetch per page.
+ * @param {number} [page=1] - The page number to fetch.
+ * @returns {Promise<void>} - A promise that resolves when the listings are fetched and displayed.
+ * @throws {Error} - Throws an error if the data cannot be fetched or displayed.
+ */
 export const displayListingsEndingSoon = async (
   sort = "endsAt",
   sortOrder = "asc",
@@ -16,8 +28,6 @@ export const displayListingsEndingSoon = async (
     listingsContainer.innerHTML = "";
     const data = await fetchAPI(listingsContainer, endpoint, queryParam);
     const listings = data?.data || [];
-
-    console.log(listings);
 
     if (!data) {
       throw new Error("Data couldn't be fetched");
--- src/js/shared/ui/auctions/auctionCards/renderAuctionCards.mjs
+++ src/js/shared/ui/auctions/auctionCards/renderAuctionCards.mjs
@@ -3,19 +3,45 @@ import { updateCountDown } from "../../../utils/auctions/updateCountDown.mjs";
 import { createAuctionCard } from "./createAuctionCard.mjs";
 
 /**
- * Creates individual auction cards
- * @param {Array<Object>} allAuctions - Array of auction objects containing auction data
- * @param {HTMLElement} containerID - The DOM element container where auction cards will be rendered
- * @param {string} allAuctions[].id - Unique identifier for the auction
- * @param {string} [allAuctions[].title] - Title of the auction item
- * @param {string} [allAuctions[].description] - Description of the auction item
- * @param {Array<Object>} [allAuctions[].media] - Array of media objects for the auction
- * @param {string} [allAuctions[].media[].url] - URL of the media file
- * @param {string} [allAuctions[].media[].alt] - Alt text for the media
- * @param {string} allAuctions[].endsAt - ISO string representing when the auction ends
- * @param {Object} [allAuctions[]._count] - Object containing count data
- * @param {number} [allAuctions[]._count.bids] - Number of bids placed on the auction
- * @returns {void}
+ * Renders auction cards into a specified container.
+ *
+ * @param {Array} allAuctions - An array of auction objects to be rendered.
+ * @param {string} containerID - The ID of the container where the auction cards will be appended.
+ * @param {string} pathname - The endpoint used to generate and navigate links for each auction item.
+ *
+ * @typedef {Object} Auction
+ * @property {string} id - The unique identifier for the auction.
+ * @property {string} [title] - The title of the auction. Defaults to "Untitled Auction" if not provided.
+ * @property {string} [description] - The description of the auction. Defaults to "No description available" if not provided.
+ * @property {Array} [media] - An array of media objects associated with the auction.
+ * @property {Object} [media[0]] - The first media object in the array.
+ * @property {string} [media[0].url] - The URL of the media. Defaults to a placeholder image if not provided.
+ * @property {string} [media[0].alt] - The alt text for the media. Defaults to "image of {title}" if not provided.
+ * @property {string} endsAt - The end date and time of the auction.
+ * @property {Object} [_count] - An object containing count-related data.
+ * @property {number} [_count.bids] - The number of bids on the auction. Defaults to "0" if not provided.
+ *
+ * @callback updateCountDown
+ * @param {string} endsAt - The end date and time of the auction.
+ * @returns {Object} - An object containing countdown data.
+ * @property {string} timeLeft - The time remaining until the auction ends.
+ * @property {boolean} isUrgent - Whether the auction is ending soon.
+ *
+ * @callback getHighestBid
+ * @param {Auction} auction - The auction object.
+ * @returns {number} - The highest bid for the auction.
+ *
+ * @callback createAuctionCard
+ * @param {string} title - The title of the auction.
+ * @param {string} description - The description of the auction.
+ * @param {string} mediaURL - The URL of the auction's media.
+ * @param {string} mediaAltText - The alt text for the auction's media.
+ * @param {string} timeLeft - The time remaining until the auction ends.
+ * @param {number} currentBid - The highest bid for the auction.
+ * @param {number} bidCount - The number of bids on the auction.
+ * @param {string} containerID - The ID of the container where the auction card will be appended.
+ * @param {string} itemLink - The URL link to the auction item.
+ * @param {boolean} isUrgent - Whether the auction is ending soon.
  */
 export const renderAuctionCards = (allAuctions, containerID, pathname) => {
   allAuctions.forEach((auction) => {
